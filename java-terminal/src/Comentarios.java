4f5388d846bd615a75ece427767ce1c9777b668a
Renato Marciano <renato_vtr@hotmail.com>
2022-10-19 18:01:49 -0300

    Terminal e Argumentos

---
+++ java-terminal/src/Comentarios.java
@@ -0,0 +1,21 @@
+public class Comentarios {
+    public static void main (String[] args) {
+        // Olá, eu sou um comentario em uma única linha
+
+        /* Olá,
+         * Eu sou um comentario
+         * que posso ser mais detalhado
+         * quando necessário
+         */
+    }
+    /**
+     * Estas duas estrelinhas acima
+     * é para identificar que você
+     * pretende elaborar um comentário
+     * a nível de documentação.
+     * Que incrível !!!
+     */
+    public void metodo(){
+
+    }
+}
