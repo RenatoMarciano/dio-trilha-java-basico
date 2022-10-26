44b95086d5db66fdb0adc88aee501b0f7c4cb84a
Renato Marciano <renato_vtr@hotmail.com>
2022-10-25 23:04:25 -0300

    Criando metodos com funções

---
+++ Operadores/src/main/java/com/example/operadores/HelloApplication.java
@@ -0,0 +1,14 @@
+package com.example.operadores;
+
+public class HelloApplication {
+
+    public static  void main(String[] args){
+
+        int i1 = 10;
+        int i2 = 10;
+
+        System.out.println("Total da soma: " + (i1 + i2));
+
+    }
+
+}
